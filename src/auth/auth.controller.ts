b984f5ec56617c2c0137d811372d05d99219475a
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-09-23 21:48:53 +0800

    nest class validator pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,15 @@ import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { get } from 'http';
 import { User, Bookmark } from 'generated/prisma';
-import { type auth } from './dto';
+import { authDto } from './dto';
+
 @Controller('auth')
 export class authController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() body: auth) {
-    console.log(body.password, body.username);
+  signup(@Body() body: authDto) {
+    console.log(body.password, body.email);
     return this.authService.signup();
   }
 
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,17 @@
-export interface auth {
-  username: String;
-  password: String;
+import {
+  IsEmail,
+  IsNotEmpty,
+  isNotEmpty,
+  IsString,
+  isString,
+} from 'class-validator';
+
+export class authDto {
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
 }
